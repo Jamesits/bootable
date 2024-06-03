f4ef796d48a5c54ed2168cfcaf1ca5712ddb261a
Jamesits <osscontrib@ciexyz.net>
2024-06-03 13:37:08 +0800

    fix dracut unable to find virtio-scsi device in initrd

--- scripts/plugin-initrd-dracut.sh
+++ scripts/plugin-initrd-dracut.sh
@@ -1,14 +1,15 @@
 #!/hint/bash
 
 DLIB_PLUGIN_INITRD_DRACUT_CAVEAT_REGENERATE_ALL=${DLIB_PLUGIN_INITRD_DRACUT_CAVEAT_REGENERATE_ALL:-1}
+DRACUT_ARGS=(--no-hostonly --no-hostonly-cmdline --no-hostonly-i18n --persistent-policy=by-uuid --fstab)
 
 dlib::plugin::initrd::generate() {
     >&2 printf "[*] dracut...\n"
 
     if [ "${DLIB_PLUGIN_INITRD_DRACUT_CAVEAT_REGENERATE_ALL}" == "1" ]; then
-        chroot "${DLIB_MOUNT_ROOT}" /bin/bash -c "set -Eeuo pipefail; export PATH=/usr/sbin:/sbin:/usr/bin:/bin:\$PATH; dracut --regenerate-all --force"
+        chroot "${DLIB_MOUNT_ROOT}" /bin/bash -c "set -Eeuo pipefail; export PATH=/usr/sbin:/sbin:/usr/bin:/bin:\$PATH; dracut --regenerate-all --force ${DRACUT_ARGS[*]}"
     else
         # - Old dracut versions does not support `--regenerate-all` https://rephlex.de/blog/2017/05/30/quick-tip-how-to-rebuild-all-initrds-with-dracut-in-centos-67/
-        chroot "${DLIB_MOUNT_ROOT}" /bin/bash -c "set -Eeuo pipefail; export PATH=/usr/sbin:/sbin:/usr/bin:/bin:\$PATH; (rpm -qa | grep kernel | grep -v headers | grep -v tools | sed 's/^kernel\-//g' | xargs -i dracut -v -f /boot/initramfs-{}.img {})"
+        chroot "${DLIB_MOUNT_ROOT}" /bin/bash -c "set -Eeuo pipefail; export PATH=/usr/sbin:/sbin:/usr/bin:/bin:\$PATH; (rpm -qa | grep kernel | grep -v headers | grep -v tools | sed 's/^kernel\-//g' | xargs -i dracut -v ${DRACUT_ARGS[*]} -f /boot/initramfs-{}.img {})"
     fi
 }
