8fa9f4bbbf78b52805e13e3796458cc8442e3370
Jamesits <osscontrib@ciexyz.net>
2024-11-10 22:57:57 +0800

    fix(misc): use new OVMF firmware path for Arch Linux

--- smoke_test.sh
+++ smoke_test.sh
@@ -9,7 +9,7 @@ if [[ "$1" == "efi" ]]; then
     qemu-system-x86_64 -no-user-config -nodefaults -msg timestamp=on -snapshot \
         -machine q35,accel=kvm -nographic -serial mon:stdio \
         -cpu host -smp 4 -m 6144 -net nic -net user \
-        -drive "if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/x64/OVMF_CODE.fd" \
+        -drive "if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/x64/OVMF_CODE.4m.fd" \
         -drive "file=${DISK},format=raw,index=0,media=disk"
 else
     qemu-system-x86_64 -no-user-config -nodefaults -msg timestamp=on -snapshot \
